fd27f87775a942acb0bfca44597459e46f85525d
riddhisourcecube <jatinappdeveloper@sourcecube.in>
2023-06-30 17:51:56 +0530

    payment details model changes

--- lib/models/paymentDeatailsModel.dart
+++ lib/models/paymentDeatailsModel.dart
@@ -7,36 +7,71 @@ class PaymentDetail {
 
   factory PaymentDetail.fromJson(Map<String, dynamic> json) {
     return PaymentDetail(
-    status : json['status'],
-    success : json['success'],
-    data : json['data'] != null ? Data.fromJson(json['data']) : null);
+        status: json['status'],
+        success: json['success'],
+        data: json['data'] != null ? Data.fromJson(json['data']) : null);
   }
 }
 
 class Data {
   String? services;
   String? subTotal;
+  String gst;
+  String hst;
+  String qst;
+  String pst;
   String? discount;
-  String? gstORhst;
-  String? pstORqst;
-  String? gstORhstInPer;
-  String? pstORqstInPer;
+  int gstInPer;
+  int hstInPer;
+  int pstInPer;
+  int qstInPer;
   String? total;
 
-  Data({this.services, this.subTotal, this.discount, this.gstORhst, this.pstORqst, this.gstORhstInPer, this.pstORqstInPer, this.total});
+  Data(
+      {this.services,
+      this.subTotal,
+      this.discount,
+      required this.gst,
+      required this.hst,
+      required this.qst,
+      required this.pst,
+      required this.gstInPer,
+      required this.hstInPer,
+      required this.pstInPer,
+      required this.qstInPer,
+      this.total});
 
   factory Data.fromJson(Map<String, dynamic> json) {
     return Data(
-    services : json['services'].toString(),
-    subTotal : json['subTotal'].toString(),
-    discount : json['discount'].toString(),
-    gstORhst : json['gstORhst'].toString(),
-    pstORqst : json['pstORqst'].toString(),
-    gstORhstInPer : json['gstORhstInPer'].toString(),
-    pstORqstInPer : json['pstORqstInPer'].toString(),
-    total : json['total'].toString(),
+      services: json['services'].toString(),
+      subTotal: json['subTotal'].toString(),
+      discount: json['discount'].toString(),
+      gst: json["GST"] ?? "",
+      hst: json["HST"] ?? "",
+      qst: json["QST"] ?? "",
+      pst: json["PST"] ?? "",
+      gstInPer: json["GstInPer"] ?? "",
+      hstInPer: json["HstInPer"] ?? "",
+      pstInPer: json["PstInPer"] ?? "",
+      qstInPer: json["QstInPer"] ?? "",
+      total: json['total'].toString(),
     );
   }
+
+  Map<String, dynamic> toMap() => {
+        "services": services,
+        "subTotal": subTotal,
+        "GST": gst,
+        "HST": hst,
+        "QST": qst,
+        "PST": pst,
+        "discount": discount,
+        "GstInPer": gstInPer,
+        "HstInPer": hstInPer,
+        "PstInPer": pstInPer,
+        "QstInPer": qstInPer,
+        "total": total,
+      };
 }
 
 // class CardDetails {
